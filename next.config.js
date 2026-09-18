f41d931b465dcc4f0cf435648059fd1012af1dc7
andstyle <andermuchael@gmail.com>
2026-09-18 14:27:14 -0300

    Corrige basePath do GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
-
 const nextConfig = {
   output: "export",
   trailingSlash: true,
-  basePath,
-  assetPrefix: basePath ? `${basePath}/` : undefined,
+  basePath: "/ESTOQUE",
+  assetPrefix: "/ESTOQUE/",
   images: { unoptimized: true },
 };
 
